68e820432325a6255b86128ba8b11797546cc505
Mateus de Campos <mateuscampos_nba@hotmail.com>
2019-04-21 13:39:16 -0300

    translate Dynamically callable types.xcplaygroundpage to pt-br

--- Whats-New-In-Swift-5-0.playground/Pages/Dynamically callable types.xcplaygroundpage/Contents.swift
+++ Whats-New-In-Swift-5-0.playground/Pages/Dynamically callable types.xcplaygroundpage/Contents.swift
@@ -1,21 +1,21 @@
 /*:
- [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+ [< Anterior](@previous)           [Home](Introduction)           [Próximo >](@next)
 
  ## Dynamically callable types
 
- [SE-0216](https://github.com/apple/swift-evolution/blob/master/proposals/0216-dynamic-callable.md) adds a new `@dynamicCallable` attribute to Swift, which brings with it the ability to mark a type as being directly callable. It’s syntactic sugar rather than any sort of compiler magic, effectively `random(numberOfZeroes: 3)` into `random.dynamicallyCall(withKeywordArguments: ["numberOfZeroes": 3])`.
+ [SE-0216](https://github.com/apple/swift-evolution/blob/master/proposals/0216-dynamic-callable.md) adiciona o novo atributo `@dynamicCallable` ao Swift, que traz consigo a abilidade de marcar um tipo como sendo directly callable. É um syntactic sugar invés de qualquer mágica de compilador, efetivamente `random(numberOfZeroes: 3)` em `random.dynamicallyCall(withKeywordArguments: ["numberOfZeroes": 3])`.
 
- `@dynamicCallable` is the natural extension of Swift 4.2's `@dynamicMemberLookup`, and serves the same purpose: to make it easier for Swift code to work alongside dynamic languages such as Python and JavaScript.
+ `@dynamicCallable` é a extensão natural do Swift 4.2's `@dynamicMemberLookup`, e serve ao mesmo proposito: fazer com que fique mais fácil para um código em Swift trabalhar juntamente com linguagens dinamicas como Python e JavaScript.
 
- To add this functionality to your own types, you need to add the `@dynamicCallable` attribute plus `func dynamicallyCall(withArguments args: [Int]) -> Double` and/or `func dynamicallyCall(withKeywordArguments args: KeyValuePairs<String, Int>) -> Double`.
+ Para adicionar essa funcionalidade para seus próprios tipos, você precisa adicionar o atributo `@dynamicCallable` mais `func dynamicallyCall(withArguments args: [Int]) -> Double` e/ou `func dynamicallyCall(withKeywordArguments args: KeyValuePairs<String, Int>) -> Double`.
 
- The first of those is used when you call the type without parameter labels (e.g. `a(b, c)`), and the second is used when you *do* provide labels (e.g. `a(b: cat, c: dog)`).
+ O primeiro deles é usado quando você chama o tipo sem o nome do parametro (e.g. `a(b, c)`), e o segundo é usando quando você provê os nomes dos parametros (e.g. `a(b: cat, c: dog)`).
 
- `@dynamicCallable` is really flexible about which data types its methods accept and return, allowing you to benefit from all of Swift’s type safety while still having some wriggle room for advanced usage. So, for the first method (no parameter labels) you can use anything that conforms to `ExpressibleByArrayLiteral` such as arrays, array slices, and sets, and for the second method (with parameter labels) you can use anything that conforms to `ExpressibleByDictionaryLiteral` such as dictionaries and key value pairs.
+ `@dynamicCallable` é realmente flexivel sobre qual tipo de dado o método aceita e retorna, permitindo voce o beneficio do Swift type safety enquanto enquanto ainda tem algum espaço de contorção para uso avançado. Então, para o primeiro método (sem nome de parametro) você consegue usar qualquer coisa que conforme com `ExpressibleByArrayLiteral` assim como arrays, pedaços de array, e sets, e para o segundo método (com nomes de parametros) você pode utilizar qualquer coisa que conforme com `ExpressibleByDictionaryLiteral` assim como dicionários e pares chave valor.
 
- As well as accepting a variety of inputs, you can also provide multiple overloads for a variety of outputs – one might return a string, one an integer, and so on. As long as Swift is able to resolve which one is used, you can mix and match all you want.
+ Assim como aceita a variedade de entradas, você também pode fornecer várias sobrecargas para uma variedade de saídas – um pode retornar uma string, outro um inteiro, e assim por diante. Enaquanto o Swift conseguir saber qual usar, você pode misturar e combinar tudo que você quiser.
 
- Let’s look at an example. Here’s a struct that generates numbers between 0 and a certain maximum, depending on what input was passed in:
+ Vamos ver um exemplo. Aqui uma struct que gera numeros entre 0 e um máximo, dependendo de qual entrada é passada:
 */
 import Foundation
 
@@ -28,14 +28,14 @@ struct RandomNumberGenerator1 {
     }
 }
 /*:
- That method can be called with any number of parameters, or perhaps zero, so we read the first value carefully and use nil coalescing to make sure there’s a sensible default.
+ Esse método pode ser chamado com qualquer numero de parametros, ou possivelmente zero, então nós lemos o primeiro valor cuidadosamente e usamos nil para que haja um padrão de merge sensato.
 
- We can now create an instance of `RandomNumberGenerator1` and call it like a function:
+ Nós podemos agora criar um instancia de `RandomNumberGenerator1` e chamar como uma função:
 */
 let random1 = RandomNumberGenerator1()
 let result1 = random1(numberOfZeroes: 0)
 /*:
- If you had used `dynamicallyCall(withArguments:)` instead – or at the same time, because you can have them both a single type – then you’d write this:
+ Se você usou `dynamicallyCall(withArguments:)` ao invés – ou ao memsmo tempo, por que você ode ter os dois no mesmo tipo – então você escrevaria isso:
 */
 @dynamicCallable
 struct RandomNumberGenerator2 {
@@ -49,19 +49,19 @@ struct RandomNumberGenerator2 {
 let random2 = RandomNumberGenerator2()
 let result2 = random2(0)
 /*:
- There are some important rules to be aware of when using `@dynamicCallable`:
+ Existem algumas regras importantes para se atentar quando usar `@dynamicCallable`:
 
- - You can apply it to structs, enums, classes, and protocols.
- - If you implement `withKeywordArguments:` and don’t implement `withArguments:`, your type can still be called without parameter labels – you’ll just get empty strings for the keys.
- - If your implementations of `withKeywordArguments:` or `withArguments:` are marked as throwing, calling the type will also be throwing.
- - You can’t add `@dynamicCallable` to an extension, only the primary definition of a type.
- - You can still add other methods and properties to your type, and use them as normal.
+ - Você pode aplicar isso a structs, enums, classes, e protocols.
+ - Se você implementar `withKeywordArguments:` e não implementar `withArguments:`, seu tipo ainda assim pode ser chamado sem nome de parametro – você vai apenas passar strings vazias para as chaves.
+ - Se suas implementações de `withKeywordArguments:` ou `withArguments:` estiverem marcadas como throwing, a chamada do método também será throwing.
+ - Você pode adicionar `@dynamicCallable` a uma estensão, apenas a definição principal de um tipo.
+ - Você ainda assim pode adicionar outros métodos e propriedades ao seu tipo, e stilizá-los normalmente.
 
- Perhaps more importantly, there is no support for method resolution, which means we must call the type directly (e.g. `random(numberOfZeroes: 5)`) rather than calling specific methods on the type (e.g. `random.generate(numberOfZeroes: 5)`). There is already some discussion on adding the latter using a method signature such as this: `func dynamicallyCallMethod(named: String, withKeywordArguments: KeyValuePairs<String, Int>)`.
+ Talvez mais importante, não há suporte para resolução de métodos, o que significa que voc^precisa chamar o tipo diretamente (e.g. `random(numberOfZeroes: 5)`) ao invés de chamar um método específico no tipo (e.g. `random.generate(numberOfZeroes: 5)`). Já existe alguma discussão sobre a adição do último usando uma assinatura de método como este: `func dynamicallyCallMethod(named: String, withKeywordArguments: KeyValuePairs<String, Int>)`.
 
- If that became possible in future Swift versions it might open up some very interesting possibilities for test mocking. In the meantime, `@dynamicCallable` is not likely to be widely popular, but it *is* hugely important for a small number of people who want interactivity with Python, JavaScript, and other languages.
+ Se isso se tornasse possível em versões futuras do Swift, isso poderia abrir algumas possibilidades muito interessantes para mockar testes. Enquanto isso, `@dynamicCallable` não é provável que seja muito popular, mas *é* enormemente importante para uma pequena quantidade de pessoas que querem interoperabilidade com Python, JavaScript, e outras linguagens.
  
  &nbsp;
 
- [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+ [< Anterior](@previous)           [Home](Introduction)           [Próximo >](@next)
  */

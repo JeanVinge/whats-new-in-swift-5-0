93a22740b917bb09e8e7d97aef0454379f6417ef
jean.vinge <jean.vinge@dextra-sw.com>
2019-04-21 19:00:40 -0300

    string interpolation

--- Whats-New-In-Swift-5-0.playground/Pages/Customizing string interpolation.xcplaygroundpage/Contents.swift
+++ Whats-New-In-Swift-5-0.playground/Pages/Customizing string interpolation.xcplaygroundpage/Contents.swift
@@ -1,37 +1,37 @@
 /*:
- [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+ [< Anterior](@previous)           [Home](Introduction)           [Próximo >](@next)
 
- ## Customizing string interpolation
+ ## Customizando string interpolation
 
- [SE-0228](https://github.com/apple/swift-evolution/blob/master/proposals/0228-fix-expressiblebystringinterpolation.md) dramatically revamped Swift’s string interpolation system so that it’s more efficient and more flexible, and it’s creating a whole new range of features that were previously impossible.
+ [SE-0228](https://github.com/apple/swift-evolution/blob/master/proposals/0228-fix-expressiblebystringinterpolation.md) reformulou dramaticamente o sistema de *string interpolation* do Swift, tornando-o mais eficiente e flexível, e criando uma nova gama de recursos antes impossíveis.
 
- In its most basic form, the new string interpolation system lets us control how objects appear in strings. Swift has default behavior for structs that is helpful for debugging, because it prints the struct name followed by all its properties. But if you were working with classes (that don’t have this behavior), or wanted to format that output so it could be user-facing, then you could use the new string interpolation system.
+ Em sua forma mais básica, o novo sistema de *string interpolation* nos permite controlar como os objetos aparecem nas strings. Swift tem comportamento padrão para estruturas que é útil para *debug*, porque ele mostra o nome da estrutura seguido por todas as suas propriedades. Mas se você estava trabalhando com classes (que não têm esse comportamento), ou queria formatar essa saída para que pudesse ser voltada para o usuário, então você poderia usar o novo sistema de *string interpolation*.
 
- For example, if we had a struct like this:
+ Por exemplo, se tivessemos uma struct como essa:
 */
 struct User {
     var name: String
     var age: Int
 }
 /*:
- If we wanted to add a special string interpolation for that so that we printed users neatly, we would add an extension to `String.StringInterpolation` with a new `appendInterpolation()` method. Swift already has several of these built in, and uses the interpolation *type* – in this case `User` to figure out which method to call.
+ Se nós quiséssemos adicionar uma *string interpolation* especial para que nós mostrarmos os usuários perfeitamente, adicionaríamos uma extensão a String.StringInterpolation com um novo método `appendInterpolation()`. O Swift já possui vários destes, e usa a *interpolation* *type* - nesse caso, `User` para descobrir qual método chamar.
 
- In this case, we’re going to add an implementation that puts the user’s name and age into a single string, then calls one of the built-in `appendInterpolation()` methods to add that to our string, like this:
+ Neste caso, vamos adicionar uma implementação que coloca o nome e a idade do usuário em uma única string e, em seguida, chama um dos métodos `appendInterpolation()` internos para adicionar isso à nossa string, assim:
 */
 extension String.StringInterpolation {
     mutating func appendInterpolation(_ value: User) {
-        appendInterpolation("My name is \(value.name) and I'm \(value.age)")
+        appendInterpolation("Meu nome é \(value.name) e eu tenho \(value.age) anos")
     }
 }
 /*:
- Now we can create a user and print out their data:
+  Agora podemos criar um usuário e mostrar seus dados:
 */
  let user = User(name: "Guybrush Threepwood", age: 33)
- print("User details: \(user)")
+ print("Detalhes do usuário: \(user)")
 /*:
- That will print **User details: My name is Guybrush Threepwood and I'm 33**, whereas with the custom string interpolation it would have printed **User details: User(name: "Guybrush Threepwood", age: 33)**. Of course, that functionality is no different from just implementing the `CustomStringConvertible` protocol, so let’s move on to more advanced usages.
+ Isso vai imprimir **Detalhes do usuário: Meu nome é Guybrush Threepwood e tenho 33**, enquanto que com a *string interpolation* personalizada teria sido mosrrado **Detalhes do usuário: Usuário (nome: "Guybrush Threepwood", idade: 33)** É claro que essa funcionalidade não é diferente de apenas implementar o protocolo `CustomStringConvertible`, então vamos passar para usos mais avançados.
 
- Your custom interpolation method can take as many parameters as you need, labeled or unlabeled. For example, we could add an interpolation to print numbers using various styles, like this:
+ Seu método de *interpolation* personalizado pode levar quantos parâmetros forem necessários, rotulados ou não rotulados. Por exemplo, poderíamos adicionar uma *interpolation* para mostrar números usando vários estilos, como este:
 */
 import Foundation
 
@@ -46,13 +46,13 @@ extension String.StringInterpolation {
     }
 }
 /*:
- The `NumberFormatter` class has a number of styles, including currency ($72.83), ordinal (1st, 12th), and spell out (five, forty-three). So, we could create a random number and have it spelled out into a string like this:
+ A classe `NumberFormatter` tem vários estilos, incluindo moeda ($ 72.83), ordinal (1st, 12th) e spell out (cinco, quarenta e três). Então, podemos criar um número aleatório e soletrá-lo em uma string como esta:
 */
  let number = Int.random(in: 0...100)
- let lucky = "The lucky number this week is \(number, style: .spellOut)."
+ let lucky = "O número da sorte é \(number, style: .spellOut)."
  print(lucky)
 /*:
- You can call `appendLiteral()` as many times as you need, or even not at all if necessary. For example, we could add a string interpolation to repeat a string multiple times, like this:
+Você pode chamar `appendLiteral()` quantas vezes precisar, ou mesmo não, se necessário. Por exemplo, poderíamos adicionar uma *string interpolation* para repetir uma string várias vezes, assim:
 */
 extension String.StringInterpolation {
     mutating func appendInterpolation(repeat str: String, _ count: Int) {
@@ -64,7 +64,7 @@ extension String.StringInterpolation {
 
 print("Baby shark \(repeat: "doo ", 6)")
 /*:
- And, as these are just regular methods, you can use Swift’s full range of functionality. For example, we might add an interpolation that joins an array of strings together, but if that array is empty execute a closure that returns a string instead:
+E como esses são apenas métodos regulares, você pode usar toda a gama de funcionalidades do Swift. Por exemplo, podemos adicionar uma *interpolation* que une uma matriz de strings, mas se essa matriz estiver vazia, execute uma *closure* que retorne uma string:
 */
 extension String.StringInterpolation {
     mutating func appendInterpolation(_ values: [String], empty defaultValue: @autoclosure () -> String) {
@@ -79,21 +79,21 @@ extension String.StringInterpolation {
 let names = ["Harry", "Ron", "Hermione"]
 print("List of students: \(names, empty: "No one").")
 /*:
- Using `@autoclosure` means that we can use simple values or call complex functions for the default value, but none of that work will be done unless `values.count` is zero.
+ Usar `@autoclosure` significa que podemos usar valores simples ou chamar funções complexas para o valor padrão, mas nada disso será feito a menos que `values.count` seja zero.
 
- With a combination of the `ExpressibleByStringLiteral` and `ExpressibleByStringInterpolation` protocols it’s now possible to create whole types using string interpolation, and if we add `CustomStringConvertible` we can even make those types print as strings however we want.
+ Com uma combinação dos protocolos `ExpressibleByStringLiteral` e `ExpressibleByStringInterpolation` agora é possível criar tipos inteiros usando a interpolação de strings e, se adicionarmos `CustomStringConvertible`, podemos até fazer esses tipos serem impressos como strings da maneira que quisermos.
 
- To make this work, we need to fulfill some specific criteria:
+ Para fazer isso funcionar, precisamos cumprir alguns critérios específicos:
 
- - Whatever type we create should conform to `ExpressibleByStringLiteral`, `ExpressibleByStringInterpolation`, and `CustomStringConvertible`. The latter is only needed if you want to customize the way the type is printed.
- - *Inside* your type needs to be a nested struct called `StringInterpolation` that conforms to `StringInterpolationProtocol`.
- - The nested struct needs to have an initializer that accepts two integers telling us roughly how much data it can expect.
- - It also needs to implement an `appendLiteral()` method, as well as one or more `appendInterpolation()` methods.
- - Your main type needs to have two initializers that allow it to be created from string literals and string interpolations.
+ - Qualquer tipo que criamos deve estar de acordo com o `ExpressibleByStringLiteral`, o` ExpressibleByStringInterpolation` e o `CustomStringConvertible`. Este último é necessário apenas se você quiser personalizar a forma como o tipo é mostrado.
+ - *Dentro* do seu tipo precisa ser uma estrutura aninhada chamada `StringInterpolation` que esteja de acordo com o `StringInterpolationProtocol`.
+ - A estrutura aninhada precisa ter um inicializador que aceite dois inteiros nos informando aproximadamente quantos dados ela pode esperar.
+ - Ele também precisa implementar um método `appendLiteral()`, assim como um ou mais métodos `appendInterpolation()`.
+ - Seu tipo principal precisa ter dois inicializadores que permitem que ele seja criado a partir de literais de string e interpolações de string.
 
- We can put all that together into an example type that can construct HTML from various common elements. The “scratchpad” inside the nested `StringInterpolation` struct will be a string: each time a new literal or interpolation is added, we’ll append it to the string. To help you see exactly what’s going on, I’ve added some `print()` calls inside the various append methods.
+ Podemos juntar tudo isso em um tipo de exemplo que pode construir HTML a partir de vários elementos comuns. O "bloco de rascunho" dentro da estrutura aninhada `StringInterpolation` será uma string: cada vez que um novo literal ou *interpolation* for adicionado, nós o anexaremos à string. Para ajudar você a ver exatamente o que está acontecendo, adicionei algumas chamadas `print()` aos vários métodos de anexação.
 
- Here’s the code.
+ Aqui está o código.
 */
 struct HTMLComponent: ExpressibleByStringLiteral, ExpressibleByStringInterpolation, CustomStringConvertible {
     struct StringInterpolation: StringInterpolationProtocol {
@@ -107,19 +107,19 @@ struct HTMLComponent: ExpressibleByStringLiteral, ExpressibleByStringInterpolati
 
         // a hard-coded piece of text – just add it
         mutating func appendLiteral(_ literal: String) {
-            print("Appending \(literal)")
+            print("Adicionando \(literal)")
             output.append(literal)
         }
 
         // a Twitter username – add it as a link
         mutating func appendInterpolation(twitter: String) {
-            print("Appending \(twitter)")
+            print("Adicionando \(twitter)")
             output.append("<a href=\"https://twitter/\(twitter)\">@\(twitter)</a>")
         }
 
         // an email address – add it using mailto
         mutating func appendInterpolation(email: String) {
-            print("Appending \(email)")
+            print("Adicionando \(email)")
             output.append("<a href=\"mailto:\(email)\">\(email)</a>")
         }
     }
@@ -138,14 +138,14 @@ struct HTMLComponent: ExpressibleByStringLiteral, ExpressibleByStringInterpolati
     }
 }
 /*:
- We can now create and use an instance of `HTMLComponent` using string interpolation like this:
+Agora podemos criar e usar uma instância de `HTMLComponent` usando a *string interpolation* como esta:
 */
- let text: HTMLComponent = "You should follow me on Twitter \(twitter: "twostraws"), or you can email me at \(email: "paul@hackingwithswift.com")."
+ let text: HTMLComponent = "Você deveria me seguir no twitter \(twitter: "twostraws"), ou me mandar email em \(email: "paul@hackingwithswift.com")."
  print(text)
 /*:
- Thanks to the `print()` calls that were scattered inside, you’ll see exactly how the string interpolation functionality works: you’ll see “Appending You should follow me on Twitter”, “Appending twostraws”, “Appending , or you can email me at “, “Appending paul@hackingwithswift.com”, and finally “Appending .” – each part triggers a method call, and is added to our string.
+ Graças às chamadas `print()` que foram espalhadas por dentro, você verá exatamente como funciona a funcionalidade de *string interpolation*: você verá “Anexando Você deve me seguir no Twitter”, “Anexando twostraws”, “Adicionando você pode me enviar um e-mail em “,“ Adicionando paul@hackingwithswift.com ”e, finalmente,“ Adicionando. ”- cada parte aciona uma chamada de método e é adicionada à nossa string.
 
  &nbsp;
 
- [< Previous](@previous)           [Home](Introduction)           [Next >](@next)
+ [< Anterior](@previous)           [Home](Introduction)           [Próximo >](@next)
  */
